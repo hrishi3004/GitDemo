0eef91e49586e15e379e24a8d33efe69989013f2
hrishi <hrishig1010@gmail.com>
2024-01-08 17:45:06 +0530

    American development team

--- src/TestNG_Practice/annotations.java
+++ src/TestNG_Practice/annotations.java
@@ -8,14 +8,19 @@ public class annotations {
 	void method(){
 		System.out.println("first method");
 		System.out.println("1st method");
+		System.out.println("*******************************");
 		System.out.println("second method");
 		System.out.println("2nd method");
+		System.out.println("*******************************");
 		System.out.println("third method");
 		System.out.println("3rd method");
+		System.out.println("*******************************");
 		System.out.println("fourth method");
 		System.out.println("4th method");
+		System.out.println("*******************************");
 		System.out.println("fifth method");
 		System.out.println("5th method");
+		System.out.println("*******************************");
 		System.out.println("sixth method");
 		System.out.println("6th method");
 	}
