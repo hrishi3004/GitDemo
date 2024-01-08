a9f11157e1e83eb072f174a8077fef390f17cc72
hrishi <hrishig1010@gmail.com>
2024-01-08 17:29:11 +0530

    Architect Asian development

--- src/TestNG_Practice/annotations.java
+++ src/TestNG_Practice/annotations.java
@@ -7,11 +7,16 @@ public class annotations {
 	@Test
 	void method(){
 		System.out.println("first method");
+		System.out.println("1st method");
 		System.out.println("second method");
+		System.out.println("2nd method");
 		System.out.println("third method");
-		
+		System.out.println("3rd method");
 		System.out.println("fourth method");
+		System.out.println("4th method");
 		System.out.println("fifth method");
+		System.out.println("5th method");
 		System.out.println("sixth method");
+		System.out.println("6th method");
 	}
 }
